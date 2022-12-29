efb750e476b08de53e3a77e8caf064eedf86e0e1
107360111 <charlie37198731@gmail.com>
2022-12-29 17:39:41 +0800

    first

---
+++ test_swift/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  test_swift
+//
+//  Created by mmslab406-mini2018-2 on 2022/12/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    func test() {
+        
+    }
+
+}
+
